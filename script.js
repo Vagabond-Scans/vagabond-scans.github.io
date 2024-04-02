02fc8abcffed754ee00a7b2426089212661ebc19
Adnanmohdk <88819169+Adnanmohamedk@users.noreply.github.com>
2024-04-02 16:10:37 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -2,3 +2,10 @@ function toggleNavbar() {
   var navbar = document.getElementById('navbar');
   navbar.classList.toggle('toggled');
 }
+
+function toggle() {
+  var searchToggle = document.getElementById('search'),
+  icon = document.getElementById('icon');
+  icon.classList.toggle('popup');
+  searchToggle.classList.toggle('popup');
+}
